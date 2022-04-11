675be3875586405a1febeeeb4b641bae51a913e8
fangxiaoxin <siyu.fang@360gst.com>
2022-04-11 15:02:30 +0800

    增加vant

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// 全局注册vant组件
+import { Button } from 'vant';
+
+
+createApp(App).use(store).use(router)
+.use(Button)
+.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h2>Vant UI</h2>
+    <br />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <br />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+<style scope>
+h2{
+  font-size: 30px;
+}
+  
+</style>
